a302c6106b7c1a65981995d9ddbce06364cf27e3
ElifAcikgoez <elifnuracikgoez@gmail.com>
2022-07-25 13:07:24 +0200

    forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GettingstartedComponent } from './gettingstarted/gettingstarted.component';
+import { CreateComponent } from './parameters/create/create.component';
+import { UpdateComponent } from './parameters/update/update.component';
+import { ReadComponent } from './parameters/read/read.component';
+import { DeleteComponent } from './parameters/delete/delete.component';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component: AppComponent, pathMatch: 'full'},
+  { path: 'create', component: CreateComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: 'data/:id', component: UpdateComponent },
+  { path: 'read', component: ReadComponent },
+  { path: 'read/:id', component: ReadComponent },
+  { path: 'delete', component: DeleteComponent },
+  { path: 'delete/:id', component: DeleteComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,49 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StepperComponent } from './stepper/stepper.component';
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import { GettingstartedComponent } from './gettingstarted/gettingstarted.component';
+import { RouteplaningComponent } from './routeplaning/routeplaning.component';
+import { ComparisonComponent } from './comparison/comparison.component';
+import {MatSelectModule} from "@angular/material/select";
+import { HttpClientModule } from '@angular/common/http';
+import { CreateComponent } from './parameters/create/create.component';
+import { ReadComponent } from './parameters/read/read.component';
+import { UpdateComponent } from './parameters/update/update.component';
+import { DeleteComponent } from './parameters/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepperComponent,
+    GettingstartedComponent,
+    RouteplaningComponent,
+    ComparisonComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    HttpClientModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
