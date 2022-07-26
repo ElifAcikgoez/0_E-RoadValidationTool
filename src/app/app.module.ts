83bea213cba178212df1ae53df048b529f879b77
ElifAcikgoez <elifnuracikgoez@gmail.com>
2022-07-26 23:47:34 +0200

    create mit material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MaterialModule} from './material-module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StepperComponent } from './stepper/stepper.component';
 import {MatStepperModule} from "@angular/material/stepper";
-import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatFormFieldControl, MatFormFieldModule} from "@angular/material/form-field";
 import {ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
 import { GettingstartedComponent } from './gettingstarted/gettingstarted.component';
 import { RouteplaningComponent } from './routeplaning/routeplaning.component';
 import { ComparisonComponent } from './comparison/comparison.component';
-import {MatSelectModule} from "@angular/material/select";
 import { HttpClientModule } from '@angular/common/http';
 import { CreateComponent } from './parameters/create/create.component';
 import { ReadComponent } from './parameters/read/read.component';
 import { UpdateComponent } from './parameters/update/update.component';
 import { DeleteComponent } from './parameters/delete/delete.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -31,6 +30,7 @@ import { DeleteComponent } from './parameters/delete/delete.component';
     ReadComponent,
     UpdateComponent,
     DeleteComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -38,13 +38,15 @@ import { DeleteComponent } from './parameters/delete/delete.component';
     MatStepperModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
     BrowserAnimationsModule,
     MatSelectModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule,
+    MatInputModule,
+
 
   ],
+
 
   providers: [],
   bootstrap: [AppComponent]
--- src/app/parameters/create/create.component.ts
+++ src/app/parameters/create/create.component.ts
@@ -5,13 +5,25 @@ import { Data } from '../../shared/data';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormControl, FormGroupDirective, NgForm, } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
+// Error when invalid control is dirty, touched, or submitted
+
 export class CreateComponent implements OnInit {
-  form: FormGroup;
+
+  klasse_v = '';
+  achse_v= '';
+  diesel_v = '';
+  schadstoff_v ='';
+
+  klassen_array: any[] = ['< 7,5t (nicht mautpflichtig)','7,5t - 11,99t','12t - 18t','>18t']
+  achsen_array: any[] = ['1','2','3','4','5','> 5']
+  schadstoff_array: any[]= ['Euro 0', 'Euro 1', 'Euro 2','Euro 3','Euro 4','Euro 5','Euro 6']
 
   constructor(private route: ActivatedRoute,
               private bs: BackendService,
@@ -20,6 +32,13 @@ export class CreateComponent implements OnInit {
              ) {
 
 
+
+
+  }
+  form!: FormGroup;
+  data! : Data;
+
+  ngOnInit(): void {
     this.form = this.fb.group(
       {
         achseControl: ['', Validators.required],
@@ -28,12 +47,14 @@ export class CreateComponent implements OnInit {
         gewichtControl: ['', Validators.required],
       }
     );
-
   }
-  data! : Data;
 
-  ngOnInit(): void {
-
+  onFormSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      return;
+    }
   }
 
   create(): void {
