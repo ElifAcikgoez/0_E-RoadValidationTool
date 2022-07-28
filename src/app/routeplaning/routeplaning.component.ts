79ab5fbe8f2ae8876182fbe63eeb5f8593de9141
ElifAcikgoez <elifnuracikgoez@gmail.com>
2022-07-28 21:11:39 +0200

    map in routeplaning component visible

--- src/app/routeplaning/routeplaning.component.ts
+++ src/app/routeplaning/routeplaning.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Loader} from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-routeplaning',
@@ -10,6 +11,17 @@ export class RouteplaningComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const loader = new Loader({
+      apiKey: 'AIzaSyDbsGq82noLvR6R3I89AN3oQmHBD6XLDoM'
+    });
+    loader.load().then(() => {
+      // @ts-ignore
+      // tslint:disable-next-line:no-unused-expression
+      new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 51.233334, lng: 6.78333},
+        zoom: 6
+      });
+    });
   }
 
 }
