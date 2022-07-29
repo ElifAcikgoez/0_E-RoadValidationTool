b513130db341f4f4aa11ec331cd4727466112bc3
ElifAcikgoez <elifnuracikgoez@gmail.com>
2022-07-29 23:38:43 +0200

    route auf karte sichtbar

--- src/app/routeplaning/routeplaning.component.ts
+++ src/app/routeplaning/routeplaning.component.ts
@@ -16,18 +16,29 @@ export class RouteplaningComponent implements OnInit {
   options: any = {
     componentRestrictions: { country: 'DE' }
   }
+  private directionsRenderer: any;
+
+
 
   constructor(
       private mapsAPILoader: MapsAPILoader
+
   ) { }
   ngOnInit(): void {
     this.mapsAPILoader.load().then(() =>{
       this.isApiLoaded = true
-      new google.maps.Map(<HTMLInputElement>document.getElementById('map'), {
+       let map =  new google.maps.Map(<HTMLInputElement>document.getElementById('map'), {
         center: {lat: 51.233334, lng: 6.78333},
         zoom: 6,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       });
+      this.directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers: true});
+      this.directionsRenderer.setOptions({
+        polylineOptions: {
+          strokeColor: 'blue'
+        }
+      });
+      this.directionsRenderer.setMap(map);
     })
 
   }
@@ -38,8 +49,9 @@ export class RouteplaningComponent implements OnInit {
   }
 
   calcRoute(){
+
     let directionsService = new google.maps.DirectionsService();
-    let directionsDisplay = new google.maps.DirectionsRenderer();
+    const directionsDisplay = this.directionsRenderer;
     var request = {
       origin: (<HTMLInputElement>document.getElementById("start")).value,
       destination: (<HTMLInputElement>document.getElementById("ziel")).value,
@@ -55,13 +67,12 @@ export class RouteplaningComponent implements OnInit {
         output.innerHTML = "<div class='alert-info'>From: " + (<HTMLInputElement>document.getElementById("start")).value + ".<br />To: " +(<HTMLInputElement>document.getElementById("ziel")).value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
 
         //display route
+        // @ts-ignore
         directionsDisplay.setDirections(result);
       } else {
         //delete route from map
         directionsDisplay.setDirections({ routes: [] });
         //center map in London
-
-
         //show error message
         // @ts-ignore
       }
